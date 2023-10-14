c9c53f97afadaec50f1fa65a71011464994af20b
sitnic-a <admir.hhs@gmail.com>
2023-10-15 00:33:39 +0200

    Models-Controller Initial --- (to change)

---
+++ MentalHealthBlogAPI/Controllers/PostController.cs
@@ -0,0 +1,26 @@
+﻿using MentalHealthBlogAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MentalHealthBlogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Post> GetAllPosts()
+        {
+            var posts = Data.GeneratePosts();
+            return posts;
+        }
+
+        [HttpGet("{id}")]
+        public Post GetById(int id)
+        {
+            var post = Data.GeneratePosts().FirstOrDefault(p => p.Id == id);
+            return post != null ? post : new Post(string.Empty, "Post doens't exist", 0);
+        }
+               
+    }
+}
