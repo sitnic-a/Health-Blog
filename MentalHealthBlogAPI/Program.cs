df67ffefe385d4c734b5dc1978fd59afa35179a9
Forder23 <admirsitnic@hotmail.com>
2023-10-18 12:34:53 +0200

    Changes on PostService and database-to complete

--- MentalHealthBlogAPI/Program.cs
+++ MentalHealthBlogAPI/Program.cs
@@ -1,5 +1,8 @@
 using MentalHealthBlogAPI.Data;
+using MentalHealthBlogAPI.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,18 +12,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Custom service registration
+builder.Services.AddScoped<IPostService, PostService>();
+
 builder.Services.AddDbContext<DataContext>(options =>
 {
+
     options.UseSqlServer(builder
         .Configuration
         .GetConnectionString("DevelopmentConnection"));
+
+    options.UseSqlServer(builder
+        .Configuration
+        .GetConnectionString("DevelopmentConnectionExpress"));
 });
 
-
-
 var app = builder.Build();
-
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ MentalHealthBlogAPI/Services/IPostService.cs
@@ -0,0 +1,12 @@
+﻿using MentalHealthBlogAPI.Models;
+
+namespace MentalHealthBlogAPI.Services
+{
+    public interface IPostService
+    {
+        public Task<IEnumerable<Post>> GetPosts();
+        public Task<Post> GetById(int id);
+        public Task<Post> Add(Post post);
+        public Task<Post> Update(int id, Post post);
+    }
+}
