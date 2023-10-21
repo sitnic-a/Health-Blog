58370795c1843b6a48a9cbe430f120d94233d71f
sitnic-a <admir.hhs@gmail.com>
2023-10-21 13:00:00 +0200

    Data seed on creation(dummy) - to change

--- MentalHealthBlogAPI/Data/DataContext.cs
+++ MentalHealthBlogAPI/Data/DataContext.cs
@@ -8,7 +8,27 @@ namespace MentalHealthBlogAPI.Data
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
         }
-            public DbSet<User> Users { get; set; }
-            public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>().HasData(
+                new { Id = 1, Username = "test_01", PasswordHash = "TT1" },
+                new { Id = 2, Username = "test_02", PasswordHash = "TT2" },
+                new { Id = 3, Username = "test_03", PasswordHash = "TT3" },
+                new { Id = 4, Username = "test_04", PasswordHash = "TT4" }
+                );
+
+            modelBuilder.Entity<Post>().HasData(
+                new { Id = 1, Title = "Title_01" , Content = "Content_T01" , UserId = 1},
+                new { Id = 2, Title = "Title_02" , Content = "Content_T02" , UserId = 1},
+                new { Id = 3, Title = "Title_03" , Content = "Content_T03" , UserId = 2},
+                new { Id = 4, Title = "Title_04" , Content = "Content_T04" , UserId = 2},
+                new { Id = 5, Title = "Title_05" , Content = "Content_T05" , UserId = 1}
+                );
+        }
     }
 }
---
+++ MentalHealthBlogAPI/Migrations/20231021094205_Seed_Data.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MentalHealthBlogAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Seed_Data : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "PasswordHash", "Username" },
+                values: new object[,]
+                {
+                    { 1, "TT1", "test_01" },
+                    { 2, "TT2", "test_02" },
+                    { 3, "TT3", "test_03" },
+                    { 4, "TT4", "test_04" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "Title", "UserId" },
+                values: new object[,]
+                {
+                    { 1, "Content_T01", "Title_01", 1 },
+                    { 2, "Content_T02", "Title_02", 1 },
+                    { 3, "Content_T03", "Title_03", 2 },
+                    { 4, "Content_T04", "Title_04", 2 },
+                    { 5, "Content_T05", "Title_05", 1 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Posts",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
