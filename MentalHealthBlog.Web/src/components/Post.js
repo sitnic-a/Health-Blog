3832f65eb14de5174ae96cb280f435d4114e2aa0
sitnic-a <admir.hhs@gmail.com>
2023-11-03 19:56:06 +0100

    Introduced react-icons and improved UI- to change

--- MentalHealthBlog.Web/src/components/Post.js
+++ MentalHealthBlog.Web/src/components/Post.js
@@ -1,27 +1,32 @@
 import React from 'react'
+import { MdOutlineModeEditOutline, MdOutlineDelete } from 'react-icons/md'
 
 export const Post = (props) => {
   return (
     <section className="post-container">
-      <div className="post-header">
-        <h1>{props.title}</h1>
+      <section className="post-container-content">
+        <div className="post-header">
+          <h1>{props.title}</h1>
+        </div>
+        <div className="post-information">
+          <textarea
+            className="post-content"
+            name="content"
+            cols={30}
+            rows={13}
+            value={props.content}
+            // disabled={true}
+          ></textarea>
+        </div>
+      </section>
+      <div className="overlay-mask">
+        <button data-action-update="update" type="button">
+          <MdOutlineModeEditOutline onClick={() => alert('Edit')} />
+        </button>
+        <button data-action-delete="delete" type="button">
+          <MdOutlineDelete onClick={() => alert('Delete')} />
+        </button>
       </div>
-      <div className="post-information">
-        <textarea
-          className="post-content"
-          name="content"
-          cols={30}
-          rows={12}
-          value={props.content}
-          disabled={true}
-        ></textarea>
-      </div>
-      <button data-action-update="update" type="button">
-        Checkmark
-      </button>
-      <button data-action-delete="delete" type="button">
-        X
-      </button>
     </section>
   )
 }
