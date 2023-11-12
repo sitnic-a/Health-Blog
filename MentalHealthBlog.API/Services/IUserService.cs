3c93ca78b68f999495e29d0eff5b1e2db9a5957d
sitnic-a <admir.hhs@gmail.com>
2023-11-12 23:26:32 +0100

    Introduced UserService, changed database - to complete

---
+++ MentalHealthBlog.API/Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using MentalHealthBlogAPI.Models;
+
+namespace MentalHealthBlog.API.Services
+{
+    public interface IUserService
+    {
+        Task<User> RegisterUser(string username, string password);
+        Task<User> Login(string username, string password);
+    }
+}
