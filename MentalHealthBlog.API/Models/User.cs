86edeb53b7773e79c54d8bdc5d1cc814d17b4db2
sitnic-a <admir.hhs@gmail.com>
2023-11-13 13:35:59 +0100

    Deleted-PasswordSalt-column

--- MentalHealthBlog.API/Models/User.cs
+++ MentalHealthBlog.API/Models/User.cs
@@ -4,7 +4,6 @@
     {
         public int Id { get; set; }
         public string Username { get; set; } = string.Empty;
-        public string PasswordSalt { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
         public List<Post> Posts { get; set; }
 
