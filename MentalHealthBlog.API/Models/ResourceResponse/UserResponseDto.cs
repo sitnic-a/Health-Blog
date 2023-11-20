1e6f7eff352b80d140aa65619582750235ba03e6
sitnic-a <admir.hhs@gmail.com>
2023-11-20 13:47:20 +0100

    Login Dto changes

---
+++ MentalHealthBlog.API/Models/ResourceResponse/UserResponseDto.cs
@@ -0,0 +1,18 @@
+﻿namespace MentalHealthBlog.API.Models.ResourceResponse
+{
+    public class UserResponseDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string JWToken { get; set; } = string.Empty;
+
+        public UserResponseDto(){}
+
+        public UserResponseDto(int id, string username, string jwtoken)
+        {
+            Id = id;
+            Username = username;
+            JWToken = jwtoken;
+        }
+    }
+}
