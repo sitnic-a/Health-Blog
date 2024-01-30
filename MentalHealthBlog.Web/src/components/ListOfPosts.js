ed79930b4fbabe06193f4e1070a610077e577c5a
sitnic-a <admir.hhs@gmail.com>
2024-01-30 23:59:13 +0100

    Charts in progress

--- MentalHealthBlog.Web/src/components/ListOfPosts.js
+++ MentalHealthBlog.Web/src/components/ListOfPosts.js
@@ -13,12 +13,64 @@ import { PieGraph } from "./PieGraph";
 
 export const ListOfPosts = () => {
   let [posts, setPosts] = useState([]);
+  let [statisticsData, setStatisticsData] = useState([]);
+  let [pieGraphData, setPieGraphData] = useState({});
+
   let location = useLocation();
 
   let loggedUser = location.state.loggedUser;
   let url = `${application.application_url}/post?UserId=${loggedUser.id}`;
+  let statisticsUrl = `${application.application_url}/statistics/pie`;
 
+  let labels;
+  let numberOfTags;
+  let d;
   useEffect(() => {
+    let getStatistics = async (url) => {
+      try {
+        await fetch(url)
+          .then((response) => response.json())
+          .then((data) => {
+            setStatisticsData(data.serviceResponseObject);
+            console.log(statisticsData);
+            labels = statisticsData.map((t) => t.tagName);
+            numberOfTags = statisticsData.map((t) => t.numberOfTags);
+            // d = {
+            //   labels: labels,
+            //   numberOfTags: numberOfTags,
+            // };
+            setPieGraphData({
+              labels: labels,
+              datasets: [
+                {
+                  label: "# of Votes",
+                  data: numberOfTags,
+                  backgroundColor: [
+                    "rgba(255, 99, 132, 0.2)",
+                    "rgba(54, 162, 235, 0.2)",
+                    "rgba(255, 206, 86, 0.2)",
+                    "rgba(75, 192, 192, 0.2)",
+                    "rgba(153, 102, 255, 0.2)",
+                    "rgba(255, 159, 64, 0.2)",
+                  ],
+                  borderColor: [
+                    "rgba(255, 99, 132, 1)",
+                    "rgba(54, 162, 235, 1)",
+                    "rgba(255, 206, 86, 1)",
+                    "rgba(75, 192, 192, 1)",
+                    "rgba(153, 102, 255, 1)",
+                    "rgba(255, 159, 64, 1)",
+                  ],
+                  borderWidth: 1,
+                },
+              ],
+            });
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
     let getPosts = async (url) => {
       await fetch(url, {
         method: "GET",
@@ -31,6 +83,9 @@ export const ListOfPosts = () => {
         .then((data) => setPosts(data.serviceResponseObject));
     };
     getPosts(url);
+    getStatistics(statisticsUrl);
+
+    // setPieGraphData(d);
   }, []);
 
   return (
@@ -43,7 +98,7 @@ export const ListOfPosts = () => {
           })}
         </section>
         <section className="pie-graph-main-container">
-          <PieGraph />
+          <PieGraph pieData={pieGraphData} />
         </section>
       </div>
     </>
--- MentalHealthBlog.Web/src/components/PieGraph.js
+++ MentalHealthBlog.Web/src/components/PieGraph.js
@@ -1,26 +1,40 @@
 import { useEffect, useState } from "react";
 import { application } from "../application";
+import { Pie } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 
-export const PieGraph = () => {
-  let [statisticsData, setStatisticsData] = useState([]);
-  let url = `${application.application_url}/statistics/pie`;
+ChartJS.register(ArcElement, Tooltip, Legend);
 
-  useEffect(() => {
-    let getStatistics = async (url) => {
-      try {
-        await fetch(url)
-          .then((response) => response.json())
-          .then((data) => {
-            setStatisticsData(data.serviceResponseObject);
-            console.log(statisticsData);
-          });
-      } catch (error) {
-        console.log(error);
-      }
-    };
+export const PieGraph = (props) => {
+  let arr = [{ ...props.pieData }];
+  const data = {
+    labels: arr.map((t) => t.tagName),
+    datasets: [
+      {
+        label: "# of Votes",
+        data: arr.map((t) => t.numberOfTags),
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
 
-    getStatistics(url);
-  }, []);
-  console.log(statisticsData);
-  return <section style={{ width: "100%", background: "red" }}>Test</section>;
+  let pieData = { ...props.pieData };
+  console.log("Pie Graph", pieData);
+  return <Pie data={pieData} />;
 };
