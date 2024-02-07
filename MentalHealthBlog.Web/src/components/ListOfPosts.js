132368ddcfe981613768171ed264f80ce2d9dfde
sitnic-a <admir.hhs@gmail.com>
2024-02-07 22:14:36 +0100

    Changes in ListOfPosts and Login components - refactoring with redux toolkit(state management)

--- MentalHealthBlog.Web/src/components/ListOfPosts.js
+++ MentalHealthBlog.Web/src/components/ListOfPosts.js
@@ -1,68 +1,49 @@
-import React from 'react'
-import { useEffect, useState, useReducer } from 'react'
+import React from "react";
+import { useEffect, useState, useReducer } from "react";
 //Import hooks
-import { useLocation } from 'react-router'
+import { useLocation } from "react-router";
 
 //Import custom configuration and data
-import { application } from '../application'
+import { application } from "../application";
 
 //Import components
-import { Post } from './Post'
-import { ListOfPostsHeader } from './ListOfPostsHeader'
-import { PieGraph } from './PieGraph'
+import { Post } from "./Post";
+import { ListOfPostsHeader } from "./ListOfPostsHeader";
+import { PieGraph } from "./PieGraph";
 
-import { Loader } from './Loader'
-import globalState from './utils/globalState'
+import { Loader } from "./Loader";
+import globalState from "./utils/globalState";
+import { useDispatch, useSelector } from "react-redux";
+import { getPosts } from "./redux-toolkit/features/postSlice";
 
 export const ListOfPosts = () => {
-  let { reducer, initialState } = globalState
-  let [state, dispatch] = useReducer(reducer, initialState)
-  let [posts, setPosts] = useState([])
+  let dispatch = useDispatch();
+  let { isLoading, posts } = useSelector((store) => store.post);
 
-  let location = useLocation()
-
-  let loggedUser = location.state.loggedUser
-  let url = `${application.application_url}/post?UserId=${loggedUser.id}`
+  let location = useLocation();
+  let loggedUser = location.state.loggedUser;
 
   useEffect(() => {
-    dispatch({ type: 'loading' })
-    let getPosts = async (url) => {
-      await fetch(url, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${loggedUser.token}`,
-        },
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          setPosts(data.serviceResponseObject)
-          dispatch({ type: 'successful' })
-        })
-    }
-    getPosts(url)
-  }, [])
+    dispatch(getPosts(loggedUser));
+  }, []);
+
+  if (isLoading) {
+    <Loader />;
+  }
 
   return (
     <>
-      {state.isLoading ? (
-        <Loader />
-      ) : (
-        <>
-          {' '}
-          <ListOfPostsHeader />
-          <div className="dashboard">
-            <section className="list-of-posts-main-container">
-              {posts.map((post) => {
-                return <Post key={post.id} {...post} />
-              })}
-            </section>
-            <section className="pie-graph-main-container">
-              <PieGraph />
-            </section>
-          </div>{' '}
-        </>
-      )}
+      <ListOfPostsHeader />
+      <div className="dashboard">
+        <section className="list-of-posts-main-container">
+          {posts.map((post) => {
+            return <Post key={post.id} {...post} />;
+          })}
+        </section>
+        <section className="pie-graph-main-container">
+          <PieGraph />
+        </section>
+      </div>
     </>
-  )
-}
+  );
+};
--- MentalHealthBlog.Web/src/components/redux-toolkit/features/postSlice.js
+++ MentalHealthBlog.Web/src/components/redux-toolkit/features/postSlice.js
@@ -1,13 +1,40 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { application } from "../../../application";
+
+export const getPosts = createAsyncThunk("post/", async (loggedUser) => {
+  let url = `${application.application_url}/post?UserId=${loggedUser.id}`;
+  let request = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${loggedUser.token}`,
+    },
+  });
+  let response = await request.json();
+  return response;
+});
 
 let postSlice = createSlice({
-  name: 'postSlice',
+  name: "postSlice",
   initialState: {
-    post: [],
+    posts: [],
     isLoading: false,
     isSuccessful: false,
     isFailed: false,
   },
-})
+  extraReducers: (builder) => {
+    //--- getPosts method
+    builder
+      .addCase(getPosts.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getPosts.rejected, (state) => {
+        state.isFailed = true;
+      })
+      .addCase(getPosts.fulfilled, (state, action) => {
+        state.posts = action.payload.serviceResponseObject;
+      });
+  },
+});
 
-export default postSlice.reducer
+export default postSlice.reducer;
