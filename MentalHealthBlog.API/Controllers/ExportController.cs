63d2cfccf926aacb16ed314f8979003e25fe8f32
sitnic-a <admir.hhs@gmail.com>
2024-09-11 18:43:24 +0200

    Export service - PDF generator started

---
+++ MentalHealthBlog.API/Controllers/ExportController.cs
@@ -0,0 +1,24 @@
+﻿using MentalHealthBlog.API.Models.ResourceResponse;
+using MentalHealthBlog.API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MentalHealthBlog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExportController : ControllerBase
+    {
+        readonly IExportService _exportService;
+        public ExportController(IExportService exportService)
+        {
+            _exportService = exportService;
+        }
+
+        [HttpPost]
+        public void ExportToPDF(List<PostDto> posts)
+        {
+            _exportService.ExportToPDF();
+        }
+    }
+}
---
+++ MentalHealthBlog.API/Services/IExportService.cs
@@ -0,0 +1,10 @@
+﻿using MentalHealthBlog.API.Models.ResourceResponse;
+
+namespace MentalHealthBlog.API.Services
+{
+    public interface IExportService
+    {
+        //void ExportToPDF(List<PostDto> posts);
+        void ExportToPDF();
+    }
+}
