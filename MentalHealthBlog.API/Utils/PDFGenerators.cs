f0599f996f807a83300c7b56900dc02917c446bc
sitnic-a <admir.hhs@gmail.com>
2024-09-12 17:01:16 +0200

    Changed name for exported document

--- MentalHealthBlog.API/Utils/PDFGenerators.cs
+++ MentalHealthBlog.API/Utils/PDFGenerators.cs
@@ -1,7 +1,9 @@
-﻿using System.IO;
+﻿using System.Globalization;
+using System.IO;
 using iText.Kernel.Colors;
 using iText.Kernel.Pdf;
 using iText.Layout;
+using iText.Layout.Borders;
 using iText.Layout.Element;
 using iText.Layout.Properties;
 using MentalHealthBlog.API.Models.ResourceResponse;
@@ -12,10 +14,12 @@ namespace MentalHealthBlog.API.Utils
     {
         public async Task<byte[]> CreatePdfFile(List<PostDto> posts)
         {
+                string date = DateTime.Now.ToString("ddMMyyyy");
+                string time = DateTime.Now.ToString("Hmmss");
                 string currentDir = Environment.CurrentDirectory;
                 string fileDirectory = System.IO.Directory.GetCurrentDirectory();
                 DirectoryInfo directory = new DirectoryInfo(
-                    System.IO.Path.GetFullPath(System.IO.Path.Combine(currentDir, "pdfs\\" + $"{DateTime.Now.Minute.ToString()}-{DateTime.Now.Millisecond}.pdf")));
+                    System.IO.Path.GetFullPath(System.IO.Path.Combine(currentDir, "pdfs\\" + $"exported-posts {date}-{time}.pdf")));
 
                 if (Directory.Exists(directory.Parent?.Name))
                 {
@@ -24,68 +28,69 @@ namespace MentalHealthBlog.API.Utils
 
                     Document doc = new Document(pdfDoc);
 
-                    for (int i = 0; i < posts.Count; i++)
+                for (int i = 0; i < posts.Count; i++)
+                {
+                    Div postContainer = new Div();
+                    postContainer
+                        .SetMaxWidth(200)
+                        .SetHorizontalAlignment(HorizontalAlignment.CENTER);
+
+                    Div postTitleContainer = new Div();
+                    postTitleContainer
+                        .SetMarginTop(10)
+                        .SetMarginBottom(10)
+                        .SetBorderRadius(new BorderRadius(3))
+                        .SetBackgroundColor(ColorConstants.ORANGE)
+                        .SetFontColor(ColorConstants.WHITE);
+
+                    Div postContentContainer = new Div();
+                    postContentContainer
+                        .SetMargin(5)
+                        .SetWidth(200)
+                        .SetBackgroundColor(ColorConstants.WHITE);
+
+                    Div postTagsContainer = new Div();
+                    postTagsContainer
+                        .SetMaxWidth(200)
+                        .SetPadding(5);
+
+                    string postTitleText = posts[i].Title;
+                    string postContentText = posts[i].Content;
+                    List<string> tags = posts[i].Tags;
+
+
+                    Paragraph titleParagraph = new Paragraph(postTitleText.ToString());
+                    titleParagraph
+                        .SetFontSize(10)
+                        .SetTextAlignment(TextAlignment.CENTER)
+                        .SetBackgroundColor(ColorConstants.ORANGE)
+                        .SetFontColor(ColorConstants.WHITE)
+                        .SetPadding(2);
+
+                    Paragraph contentParagraph = new Paragraph(postContentText.ToString());
+                    contentParagraph
+                        .SetFontSize(8)
+                        .SetPadding(2);
+
+                    Paragraph postTagsParagraph = new Paragraph();
+                    for (int j = 0; j < tags.Count; j++)
                     {
-                        Div postContainer = new Div();
-                        postContainer
-                            .SetMaxWidth(200)
-                            .SetHorizontalAlignment(HorizontalAlignment.CENTER);
-
-                        Div postTitleContainer = new Div();
-                        postTitleContainer
-                            .SetMarginTop(10)
-                            .SetMarginBottom(10)
-                            .SetBorderRadius(new BorderRadius(3))
-                            .SetBackgroundColor(ColorConstants.ORANGE)
-                            .SetFontColor(ColorConstants.WHITE);
-
-                        Div postContentContainer = new Div();
-                        postContentContainer
-                            .SetMargin(5)
-                            .SetWidth(200)
-                            .SetBackgroundColor(ColorConstants.WHITE);
-
-                        Div postTagsContainer = new Div();
-                        postTagsContainer
-                            .SetMaxWidth(200)
-                            .SetPadding(5);
-
-                        string postTitleText = posts[i].Title;
-                        string postContentText = posts[i].Content;
-                        List<string> tags = posts[i].Tags;
-
-
-                        Paragraph titleParagraph = new Paragraph(postTitleText.ToString());
-                        titleParagraph
-                            .SetFontSize(10)
-                            .SetTextAlignment(TextAlignment.CENTER)
-                            .SetBackgroundColor(ColorConstants.ORANGE)
-                            .SetFontColor(ColorConstants.WHITE)
-                            .SetPadding(2);
-
-                        Paragraph contentParagraph = new Paragraph(postContentText.ToString());
-                        contentParagraph
+                        postTagsParagraph
+                            .Add($@"#{tags[j]}  ")
                             .SetFontSize(8)
-                            .SetPadding(2);
+                            .SetMarginLeft(2);
+                    }
 
-                        Paragraph postTagsParagraph = new Paragraph();
-                        for (int j = 0; j < tags.Count; j++)
-                        {
-                            postTagsParagraph
-                                .Add($@"#{tags[j]}  ")
-                                .SetFontSize(8)
-                                .SetMarginLeft(2);
-                        }
+                    postTitleContainer.Add(titleParagraph);
+                    postContentContainer.Add(contentParagraph);
+                    postTagsContainer.Add(postTagsParagraph);
 
-                        postTitleContainer.Add(titleParagraph);
-                        postContentContainer.Add(contentParagraph);
-                        postTagsContainer.Add(postTagsParagraph);
+                    postContainer.Add(postTitleContainer);
+                    postContainer.Add(postContentContainer);
+                    postContainer.Add(postTagsContainer);
+                    postContainer.AddStyle(new Style().SetBorderBottom(new SolidBorder(ColorConstants.BLACK, 0.3f)));
 
-                        postContainer.Add(postTitleContainer);
-                        postContainer.Add(postContentContainer);
-                        postContainer.Add(postTagsContainer);
-
-                        doc.Add(postContainer);
+                    doc.Add(postContainer);
                     }
 
                     doc.Close();
@@ -163,6 +168,7 @@ namespace MentalHealthBlog.API.Utils
                         postContainer.Add(postTitleContainer);
                         postContainer.Add(postContentContainer);
                         postContainer.Add(postTagsContainer);
+                        postContainer.AddStyle(new Style().SetBorderBottom(new SolidBorder(ColorConstants.BLACK,0.3f)));
 
                         doc.Add(postContainer);
                     }
