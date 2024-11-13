d986f143c60d3c4549ce96873cede24ca8ba0dcf
sitnic-a <admir.hhs@gmail.com>
2024-11-13 13:56:05 +0100

    Refactored code in mental expert service

--- MentalHealthBlog.API/Services/MentalExpertService.cs
+++ MentalHealthBlog.API/Services/MentalExpertService.cs
@@ -1,7 +1,9 @@
 ﻿using MentalHealthBlog.API.Methods;
+using MentalHealthBlog.API.Models;
 using MentalHealthBlog.API.Models.ResourceRequest;
 using MentalHealthBlog.API.Models.ResourceResponse;
 using MentalHealthBlogAPI.Data;
+using MentalHealthBlogAPI.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
@@ -50,37 +52,13 @@ namespace MentalHealthBlog.API.Services
                         .Where(ex => ex.SharedWithId == query.LoggedExpertId)
                         .GroupBy(u => u.SharedPost.User);
 
-                    List<SharesPerUserDto> sharesPerUser = new List<SharesPerUserDto>();
-                    PostHelper convertHelper = new PostHelper(_context);
+                    List<SharesPerUserDto> sharesPerUser = await FillListWithGroupedUsersAndTheirShares(groupedUsersAndTheirShares);
 
-                    foreach (var userFromGroup in groupedUsersAndTheirShares)
-                    {
-                        var dbUserByKey = await _context.Users.FindAsync(userFromGroup.Key.Id);
-                        UserDto userThatSharedContent;
-                        List<PostDto> contentUserShared = new List<PostDto>();
-
-                        if (dbUserByKey is not null)
-                        {
-                            userThatSharedContent = new UserDto(dbUserByKey.Id, dbUserByKey.Username);
-                            foreach (var sharedContent in userFromGroup)
-                            {
-                                var post = sharedContent?.SharedPost;
-                                if (post is not null)
-                                {
-                                    var postTags = convertHelper.CallReturnPostTags(post.Id);
-                                    var postDto = new PostDto(post.Id, post.Title, post.Content, post.UserId, post.CreatedAt, postTags);
-                                    contentUserShared.Add(postDto);
-                                }
-                            }
-                            sharesPerUser.Add(new SharesPerUserDto(userThatSharedContent, contentUserShared));
-                        }
-                    }
                     if (!sharesPerUser.IsNullOrEmpty())
                     {
                         _mentalExpertLoggerService.LogInformation($"SHARES-PER-USER: {MentalExpertServiceLogTypes.SUCCESS.ToString()}", sharesPerUser);
                         return new Response(sharesPerUser, StatusCodes.Status200OK, MentalExpertServiceLogTypes.SUCCESS.ToString());
                     }
-
                     _mentalExpertLoggerService.LogWarning($"SHARES-PER-USER: {MentalExpertServiceLogTypes.EMPTY.ToString()}");
                     return new Response(new object(), StatusCodes.Status204NoContent, MentalExpertServiceLogTypes.EMPTY.ToString());
                 }
@@ -89,9 +67,45 @@ namespace MentalHealthBlog.API.Services
             }
             catch (Exception e)
             {
-                _mentalExpertLoggerService.LogError($"SHARES-PER-USER: {MentalExpertServiceLogTypes.ERROR.ToString()}",e);
+                _mentalExpertLoggerService.LogError($"SHARES-PER-USER: {MentalExpertServiceLogTypes.ERROR.ToString()}", e);
                 return new Response(e.Data, StatusCodes.Status400BadRequest, MentalExpertServiceLogTypes.ERROR.ToString());
             }
+        }
+
+        private async Task<List<SharesPerUserDto>> FillListWithGroupedUsersAndTheirShares(IEnumerable<IGrouping<User, Share>> groupedUsersAndTheirShares)
+        {
+            List<SharesPerUserDto> sharesPerUser = new List<SharesPerUserDto>();
+            PostHelper convertHelper = new PostHelper(_context);
+
+            if (groupedUsersAndTheirShares.IsNullOrEmpty())
+            {
+                _mentalExpertLoggerService.LogWarning($"SHARES-PER-USER: {MentalExpertServiceLogTypes.EMPTY.ToString()}");
+                return new List<SharesPerUserDto>();
+            }
+
+            foreach (var userFromGroup in groupedUsersAndTheirShares)
+            {
+                var dbUserByKey = await _context.Users.FindAsync(userFromGroup.Key.Id);
+                UserDto userThatSharedContent;
+                List<PostDto> contentUserShared = new List<PostDto>();
+
+                if (dbUserByKey is not null)
+                {
+                    userThatSharedContent = new UserDto(dbUserByKey.Id, dbUserByKey.Username);
+                    foreach (var sharedContent in userFromGroup)
+                    {
+                        var post = sharedContent?.SharedPost;
+                        if (post is not null)
+                        {
+                            var postTags = convertHelper.CallReturnPostTags(post.Id);
+                            var postDto = new PostDto(post.Id, post.Title, post.Content, post.UserId, post.CreatedAt, postTags);
+                            contentUserShared.Add(postDto);
+                        }
+                    }
+                    sharesPerUser.Add(new SharesPerUserDto(userThatSharedContent, contentUserShared));
+                }
+            }
+            return sharesPerUser;
         }
     }
 }
