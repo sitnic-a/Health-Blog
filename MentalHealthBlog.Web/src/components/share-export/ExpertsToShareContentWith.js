3221a353bccb12c3558c8d6c398f4e86acdf7459
sitnic-a <admir.hhs@gmail.com>
2024-11-27 13:08:27 +0100

    Refactoring

---
+++ MentalHealthBlog.Web/src/components/share-export/ExpertsToShareContentWith.js
@@ -0,0 +1,89 @@
+import { useSelector } from "react-redux";
+import { getPeopleToShareContentWith } from "../utils/helper-methods/methods";
+import defaultAvatar from "../../images/default-avatar.png";
+import { LiaSearchSolid } from "react-icons/lia";
+
+export const ExpertsToShareContentWith = () => {
+  let { possibleToShareWith } = useSelector((store) => store.shareExport);
+
+  return (
+    <>
+      <div className="search-people">
+        <span>
+          <LiaSearchSolid />
+        </span>
+        <input
+          type="text"
+          name="search-by-first-last-name"
+          id="search-by-first-last-name"
+          placeholder="Search by first/last name..."
+        />
+      </div>
+
+      <div className="people-to-give-permission-container">
+        <div className="people-to-give-permission">
+          {possibleToShareWith.map((personToShareWith) => {
+            return (
+              <div className="person-to-give-permission-container">
+                <div className="permission-action">
+                  <input
+                    type="checkbox"
+                    name="person-to-give-permission-checkbox"
+                    id="person-to-give-permission-checkbox"
+                    onClick={() => getPeopleToShareContentWith()}
+                  />
+                </div>
+
+                <div className="person-to-give-permission-information">
+                  <div className="person-to-give-permission-img-container">
+                    <img
+                      className="person-to-give-permission-img"
+                      src={defaultAvatar}
+                      alt="Person photo"
+                    />
+                  </div>
+                  <div className="person-to-give-permission-basic-information">
+                    <input
+                      type="hidden"
+                      name="person-permission-info"
+                      className="person-permission-info info-id"
+                      value={personToShareWith.id}
+                    />
+                    <p
+                      className="person-permission-info info-username"
+                      title={personToShareWith.username}
+                    >
+                      {personToShareWith.username}
+                    </p>
+                    <p
+                      className="person-permission-info info-role"
+                      title={personToShareWith.roles[0].name}
+                    >
+                      {personToShareWith.roles[0].name}
+                    </p>
+                    <p
+                      className="person-permission-info info-phoneNumber"
+                      title={personToShareWith.phoneNumber}
+                    >
+                      {personToShareWith.phoneNumber}
+                    </p>
+                    <hr />
+                    <p
+                      className="person-permission-info info-organization"
+                      title={personToShareWith.organization}
+                    >
+                      {personToShareWith.organization}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <p className="people-to-give-permission-count">
+          Number of persons content is shared with
+        </p>
+      </div>
+    </>
+  );
+};
---
+++ MentalHealthBlog.Web/src/components/share-export/ExportModal.js
@@ -0,0 +1,52 @@
+import { useDispatch, useSelector } from "react-redux";
+import { openExportModal } from "../redux-toolkit/features/modalSlice";
+
+export const ExportModal = () => {
+  let dispatch = useDispatch();
+
+  let { isExportOpen } = useSelector((store) => store.modal);
+  let { postsToExport, isExported } = useSelector((store) => store.shareExport);
+
+  return (
+    isExportOpen && (
+      <>
+        <section className="export-modal-overlay">
+          <section className="export-modal-container">
+            <span
+              className="share-export-close-modal-btn"
+              onClick={() => {
+                dispatch(openExportModal(!isExportOpen));
+                let shareExportContainer = document.querySelector(
+                  ".share-export-container"
+                );
+                shareExportContainer.style.display = "flex";
+              }}
+            >
+              X
+            </span>
+            <div className="export-modal">
+              <div className="export-modal-content">
+                <h4>Exporting posts...</h4>
+                <div className="export-modal-files-container">
+                  {postsToExport.map((post) => {
+                    return (
+                      <div className="export-modal-file-wrapper">
+                        <p>{post.title}</p>
+                      </div>
+                    );
+                  })}
+                </div>
+
+                {isExported && (
+                  <div className="export-modal-progress-bar">
+                    <p>Successfully exported!</p>
+                  </div>
+                )}
+              </div>
+            </div>
+          </section>
+        </section>
+      </>
+    )
+  );
+};
