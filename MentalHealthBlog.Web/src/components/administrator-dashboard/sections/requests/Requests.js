f06fac93e4e2c00b2361d13fec1a7b027b1a60da
sitnic-a <admir.hhs@gmail.com>
2025-01-05 15:34:53 +0100

    Created admin slice - frontend for displaying just created mental health experts

--- MentalHealthBlog.Web/src/components/administrator-dashboard/sections/requests/Requests.js
+++ MentalHealthBlog.Web/src/components/administrator-dashboard/sections/requests/Requests.js
@@ -1,14 +1,29 @@
-import { Link } from 'react-router-dom'
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getNewRegisteredExperts } from "../../../redux-toolkit/features/adminSlice";
+
+import { Link } from "react-router-dom";
 export const Requests = () => {
+  let dispatch = useDispatch();
+  let { numberOfNewlyRegisteredMentalHealthExperts } = useSelector(
+    (store) => store.admin
+  );
+
+  useEffect(() => {
+    dispatch(getNewRegisteredExperts());
+  }, []);
+
   return (
     <div className="requests-main-container">
       <Link
-        to={'/requests/new-experts'}
+        to={"/requests/new-experts"}
         className="request-link new-mental-health-expert-request-link"
       >
-        <span className="new-mental-health-expert-request-container-notification">
-          6
-        </span>
+        {numberOfNewlyRegisteredMentalHealthExperts && (
+          <span className="new-mental-health-expert-request-container-notification">
+            {numberOfNewlyRegisteredMentalHealthExperts}
+          </span>
+        )}
 
         <div className="new-mental-health-expert-request-container">
           <p className="new-mental-health-expert-request-container-label">
@@ -16,12 +31,14 @@ export const Requests = () => {
           </p>
           <p className="new-mental-health-expert-request-container-info">
             Newly registered:
-            <span className="new-mental-health-expert-request-container-number-of-registered">
-              6
-            </span>
+            {numberOfNewlyRegisteredMentalHealthExperts && (
+              <span className="new-mental-health-expert-request-container-number-of-registered">
+                {numberOfNewlyRegisteredMentalHealthExperts}
+              </span>
+            )}
           </p>
         </div>
       </Link>
     </div>
-  )
-}
+  );
+};
