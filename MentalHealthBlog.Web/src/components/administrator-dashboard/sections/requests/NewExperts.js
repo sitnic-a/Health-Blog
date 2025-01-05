5cfa5ed8528895ec6b971fea37f4c31e21b35361
sitnic-a <admir.hhs@gmail.com>
2025-01-05 19:32:14 +0100

    Refactor - WIP

--- MentalHealthBlog.Web/src/components/administrator-dashboard/sections/requests/NewExperts.js
+++ MentalHealthBlog.Web/src/components/administrator-dashboard/sections/requests/NewExperts.js
@@ -1,11 +1,18 @@
+import useFetchLocationState from "../../../custom/hooks/useFetchLocationState";
+
 export const NewExperts = () => {
+  let { newlyRegisteredMentalHealthExperts } = useFetchLocationState();
+  console.log(newlyRegisteredMentalHealthExperts);
+
   return (
-    <div className="new-experts-main-container">
-      {/* Service, controller for printing not approved experts */}
-      {/* Real-time update of new registered experts - SignalR */}
-      {/* Display all not approved experts with two actions - approve and reject */}
-      {/* Admin slice for cleaner code and better manipulation */}
-      {/* Create component to print mental health expert */}
-    </div>
+    newlyRegisteredMentalHealthExperts && (
+      <div className="new-experts-main-container">
+        {/* Service, controller for printing not approved experts */}
+        {/* Real-time update of new registered experts - SignalR */}
+        {/* Display all not approved experts with two actions - approve and reject */}
+        {/* Admin slice for cleaner code and better manipulation */}
+        {/* Create component to print mental health expert */}
+      </div>
+    )
   );
 };
--- MentalHealthBlog.Web/src/components/administrator-dashboard/sections/requests/Requests.js
+++ MentalHealthBlog.Web/src/components/administrator-dashboard/sections/requests/Requests.js
@@ -1,44 +1,8 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getNewRegisteredExperts } from "../../../redux-toolkit/features/adminSlice";
-
-import { Link } from "react-router-dom";
+import { RequestNewExperts } from "./RequestNewExperts";
 export const Requests = () => {
-  let dispatch = useDispatch();
-  let { numberOfNewlyRegisteredMentalHealthExperts } = useSelector(
-    (store) => store.admin
-  );
-
-  useEffect(() => {
-    dispatch(getNewRegisteredExperts());
-  }, []);
-
   return (
     <div className="requests-main-container">
-      <Link
-        to={"/requests/new-experts"}
-        className="request-link new-mental-health-expert-request-link"
-      >
-        {numberOfNewlyRegisteredMentalHealthExperts && (
-          <span className="new-mental-health-expert-request-container-notification">
-            {numberOfNewlyRegisteredMentalHealthExperts}
-          </span>
-        )}
-
-        <div className="new-mental-health-expert-request-container">
-          <p className="new-mental-health-expert-request-container-label">
-            Mental Health Experts
-          </p>
-          <p className="new-mental-health-expert-request-container-info">
-            Newly registered:
-            {numberOfNewlyRegisteredMentalHealthExperts && (
-              <span className="new-mental-health-expert-request-container-number-of-registered">
-                {numberOfNewlyRegisteredMentalHealthExperts}
-              </span>
-            )}
-          </p>
-        </div>
-      </Link>
+      <RequestNewExperts />
     </div>
   );
 };
