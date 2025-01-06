9228d65acae5d84af432cadec6fb1978f07dc68b
sitnic-a <admir.hhs@gmail.com>
2025-01-06 13:01:25 +0100

    Introduced SignalR

--- MentalHealthBlog.API/Controllers/UserController.cs
+++ MentalHealthBlog.API/Controllers/UserController.cs
@@ -1,8 +1,10 @@
 ﻿using MentalHealthBlog.API.Models.ResourceRequest;
 using MentalHealthBlog.API.Models.ResourceResponse;
 using MentalHealthBlog.API.Services;
+using MentalHealthBlog.API.Utils.SignalR;
 using MentalHealthBlogAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 
 namespace MentalHealthBlog.API.Controllers
 {
@@ -11,16 +13,23 @@ namespace MentalHealthBlog.API.Controllers
     public class UserController : ControllerBase
     {
         private readonly IUserService _userService;
+        private readonly IHubContext<AdminHub> _adminHubContext;
+        private readonly IAdminService _adminService;
 
-        public UserController(IUserService userService)
+        public UserController(IUserService userService, IHubContext<AdminHub> adminHubContext, IAdminService adminService)
         {
-            this._userService = userService;
+            _userService = userService;
+            _adminHubContext = adminHubContext;
+            _adminService = adminService;
         }
 
         [HttpPost("register")]
         public async Task<Response> Register([FromForm] CreateUserDto newUserRequest)
         {
-            return await _userService.Register(newUserRequest);
+            var registeredUser = await _userService.Register(newUserRequest);
+            var newlyRegisteredMentalHealthExperts = await _adminService.GetNewRegisteredExperts();
+            var hubResult = _adminHubContext.Clients.All.SendAsync("GetNewRegisteredMentalHealthExperts", newlyRegisteredMentalHealthExperts);
+            return registeredUser;
         }
 
         [HttpPost("login")]
