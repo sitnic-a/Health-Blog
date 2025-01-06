ad43c65283d590a658609e7af74f7cab2377d48a
sitnic-a <admir.hhs@gmail.com>
2025-01-06 20:32:17 +0100

    Minor design changes - WIP

--- MentalHealthBlog.Web/src/components/administrator-dashboard/sections/requests/NewExperts.js
+++ MentalHealthBlog.Web/src/components/administrator-dashboard/sections/requests/NewExperts.js
@@ -8,19 +8,16 @@ export const NewExperts = () => {
   console.log(newlyRegisteredMentalHealthExperts);
 
   return (
-    newlyRegisteredMentalHealthExperts && (
-      <div className="new-experts-main-container">
-        {newlyRegisteredMentalHealthExperts.map((expert) => {
-          return (
-            <NewMentalHealthExpertProfile key={expert.id} expert={expert} />
-          );
-        })}
-        {/* Service, controller for printing not approved experts */}
-        {/* Real-time update of new registered experts - SignalR */}
-        {/* Display all not approved experts with two actions - approve and reject */}
-        {/* Admin slice for cleaner code and better manipulation */}
-        {/* Create component to print mental health expert */}
-      </div>
-    )
+    <>
+      {newlyRegisteredMentalHealthExperts && (
+        <div className="new-experts-main-container">
+          {newlyRegisteredMentalHealthExperts.map((expert) => {
+            return (
+              <NewMentalHealthExpertProfile key={expert.id} expert={expert} />
+            );
+          })}
+        </div>
+      )}
+    </>
   );
 };
