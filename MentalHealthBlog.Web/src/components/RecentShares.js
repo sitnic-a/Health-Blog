830a7df1d9ed671c94d0452b883ffea7c0721cb9
sitnic-a <admir.hhs@gmail.com>
2025-02-08 17:16:50 +0100

    Refactor - WIP

---
+++ MentalHealthBlog.Web/src/components/RecentShares.js
@@ -0,0 +1,97 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+import {
+  returnSharePerMentalHealthExpertRecentShareBasicInfoElements,
+  formatDateToString,
+} from './utils/helper-methods/methods'
+
+import { FaRegSquareMinus } from 'react-icons/fa6'
+import { FaRegSquarePlus } from 'react-icons/fa6'
+
+export const RecentShares = () => {
+  let { recentShares } = useSelector((store) => store.regularUser)
+  return (
+    recentShares.length > 0 && (
+      <div className="shares-per-mental-health-expert-recent-shares-main-container">
+        <div className="shares-per-mental-health-expert-recent-shares-header">
+          <h2 className="shares-per-mental-health-expert-recent-shares-header-title">
+            Recent posts:
+          </h2>
+        </div>
+        <div className="shares-per-mental-health-expert-recent-shares-container">
+          {recentShares.map((share, index) => {
+            let basicInfoContainerHeight
+            let person = `${share.sharedWith.firstName} ${share.sharedWith.lastName}`
+            let sharedAt = formatDateToString(share.sharedPost.sharedAt)
+            return (
+              <div
+                className="shares-per-mental-health-expert-recent-share-container"
+                key={index}
+              >
+                <div className="shares-per-mental-health-expert-recent-share-title-container">
+                  <h1 className="shares-per-mental-health-expert-recent-share-title">
+                    {share.sharedPost.title}
+                  </h1>
+                  <span className="shares-per-mental-health-expert-recent-share-actions-container">
+                    <FaRegSquareMinus
+                      className="shares-per-mental-health-expert-recent-share-action recent-share-shrink-info-action"
+                      onClick={(e) => {
+                        let { expandAction, basicInfoContainer } =
+                          returnSharePerMentalHealthExpertRecentShareBasicInfoElements(
+                            e
+                          )
+
+                        basicInfoContainerHeight =
+                          basicInfoContainer.clientHeight
+                        basicInfoContainer.style.height = `0px`
+                        basicInfoContainer.style.transition = 'height 150ms'
+
+                        e.currentTarget.style.display = 'none'
+                        setTimeout(() => {
+                          basicInfoContainer.style.visibility = 'hidden'
+                          expandAction.style.display = 'inline'
+                        }, 10)
+                      }}
+                    />
+                    <FaRegSquarePlus
+                      className="shares-per-mental-health-expert-recent-share-action recent-share-expand-info-action"
+                      onClick={(e) => {
+                        let { basicInfoContainer, shrinkAction } =
+                          returnSharePerMentalHealthExpertRecentShareBasicInfoElements(
+                            e
+                          )
+
+                        basicInfoContainer.style.height = `${basicInfoContainerHeight}px`
+                        basicInfoContainer.style.transition = 'height 150ms'
+
+                        e.currentTarget.style.display = 'none'
+                        setTimeout(() => {
+                          basicInfoContainer.style.visibility = 'visible'
+                          shrinkAction.style.display = 'inline'
+                        }, 10)
+                      }}
+                    />
+                  </span>
+                </div>
+                <div className="shares-per-mental-health-expert-recent-share-basic-info-container">
+                  <p className="shares-per-mental-health-expert-recent-share-basic-info">
+                    <b>Content: </b> {share.sharedPost.content}
+                  </p>
+                  <p className="shares-per-mental-health-expert-recent-share-basic-info">
+                    <b>Chosen expert: </b>
+                    {person}
+                  </p>
+                  <p className="shares-per-mental-health-expert-recent-share-basic-info">
+                    <b>Share date: </b> {sharedAt}
+                  </p>
+                </div>
+                <hr />
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    )
+  )
+}
--- MentalHealthBlog.Web/src/components/SharedPosts.js
+++ MentalHealthBlog.Web/src/components/SharedPosts.js
@@ -2,9 +2,6 @@ import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { shareByLink } from './redux-toolkit/features/shareExportSlice'
-
-import { Loader } from './Loader'
-import { ListOfSharedPosts } from './share-export/share/ListOfSharedPosts'
 import {
   getRecentShares,
   getSharesPerMentalHealthExpert,
@@ -12,17 +9,17 @@ import {
   previewHoveredContentCounter,
 } from './redux-toolkit/features/regularUserSlice'
 import useFetchLocationState from './custom/hooks/useFetchLocationState'
-import { formatDateToString } from './utils/helper-methods/methods'
 
-import { FaRegSquareMinus } from 'react-icons/fa6'
-import { FaRegSquarePlus } from 'react-icons/fa6'
+import { Loader } from './Loader'
+import { ListOfSharedPosts } from './share-export/share/ListOfSharedPosts'
+import { RecentShares } from './RecentShares'
 
 export const SharedPosts = () => {
   let dispatch = useDispatch()
   let { loggedUser } = useFetchLocationState()
 
   let { postsToShare, isLoading } = useSelector((store) => store.shareExport)
-  let { sharesPerMentalHealthExpert, recentShares } = useSelector(
+  let { sharesPerMentalHealthExpert } = useSelector(
     (store) => store.regularUser
   )
 
@@ -115,98 +112,7 @@ export const SharedPosts = () => {
                 })}
               </div>
 
-              {recentShares.length > 0 && (
-                <div className="shares-per-mental-health-expert-recent-shares-main-container">
-                  <div className="shares-per-mental-health-expert-recent-shares-header">
-                    <h2 className="shares-per-mental-health-expert-recent-shares-header-title">
-                      Recent posts:
-                    </h2>
-                  </div>
-                  <div className="shares-per-mental-health-expert-recent-shares-container">
-                    {recentShares.map((share, index) => {
-                      let basicInfoContainerHeight
-                      let person = `${share.sharedWith.firstName} ${share.sharedWith.lastName}`
-                      let sharedAt = formatDateToString(
-                        share.sharedPost.sharedAt
-                      )
-                      return (
-                        <div
-                          className="shares-per-mental-health-expert-recent-share-container"
-                          key={index}
-                        >
-                          <div className="shares-per-mental-health-expert-recent-share-title-container">
-                            <h1 className="shares-per-mental-health-expert-recent-share-title">
-                              {share.sharedPost.title}
-                            </h1>
-                            <span className="shares-per-mental-health-expert-recent-share-actions-container">
-                              <FaRegSquareMinus
-                                className="shares-per-mental-health-expert-recent-share-action recent-share-shrink-info-action"
-                                onClick={(e) => {
-                                  let spanContainer = e.currentTarget.parentNode
-                                  let expandAction =
-                                    spanContainer.children.item(1)
-                                  let recentShareContainer =
-                                    e.currentTarget.parentNode.parentNode
-                                      .parentNode
-
-                                  let basicInfoContainer =
-                                    recentShareContainer.children.item(1)
-                                  basicInfoContainerHeight =
-                                    basicInfoContainer.clientHeight
-                                  basicInfoContainer.style.height = 0 + 'px'
-                                  basicInfoContainer.style.transition =
-                                    'height 150ms'
-
-                                  e.currentTarget.style.display = 'none'
-                                  basicInfoContainer.style.visibility = 'hidden'
-                                  expandAction.style.display = 'inline'
-                                }}
-                              />
-                              <FaRegSquarePlus
-                                className="shares-per-mental-health-expert-recent-share-action recent-share-expand-info-action"
-                                onClick={(e) => {
-                                  let spanContainer = e.currentTarget.parentNode
-                                  let shrinkAction =
-                                    spanContainer.children.item(0)
-                                  let recentShareContainer =
-                                    e.currentTarget.parentNode.parentNode
-                                      .parentNode
-
-                                  let basicInfoContainer =
-                                    recentShareContainer.children.item(1)
-                                  basicInfoContainer.style.height =
-                                    basicInfoContainerHeight + 'px'
-                                  basicInfoContainer.style.transition =
-                                    'height 150ms'
-
-                                  e.currentTarget.style.display = 'none'
-                                  basicInfoContainer.style.visibility =
-                                    'visible'
-
-                                  shrinkAction.style.display = 'inline'
-                                }}
-                              />
-                            </span>
-                          </div>
-                          <div className="shares-per-mental-health-expert-recent-share-basic-info-container">
-                            <p className="shares-per-mental-health-expert-recent-share-basic-info">
-                              <b>Content: </b> {share.sharedPost.content}
-                            </p>
-                            <p className="shares-per-mental-health-expert-recent-share-basic-info">
-                              <b>Chosen expert: </b>
-                              {person}
-                            </p>
-                            <p className="shares-per-mental-health-expert-recent-share-basic-info">
-                              <b>Share date: </b> {sharedAt}
-                            </p>
-                          </div>
-                          <hr />
-                        </div>
-                      )
-                    })}
-                  </div>
-                </div>
-              )}
+              <RecentShares />
             </section>
           </section>
         )
