91bb6a65d4bbdd8ad3b7c555830987f53f197ca5
sitnic-a <admir.hhs@gmail.com>
2025-02-13 21:40:17 +0100

    Improving add post feature to add emotions and topics

---
+++ MentalHealthBlog.API/Services/EmotionService.cs
@@ -0,0 +1,44 @@
+﻿using MentalHealthBlog.API.Models.ResourceResponse;
+using MentalHealthBlogAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace MentalHealthBlog.API.Services
+{
+    enum EmotionServiceLogTypes
+    {
+        EMPTY_OR_NULL_TABLE,
+        NOT_FOUND,
+        SUCCESS,
+        ERROR
+    }
+    public class EmotionService : IEmotionService
+    {
+        private readonly DataContext _context;
+        private readonly ILogger<IEmotionService> _emotionLoggerService;
+
+        public EmotionService(DataContext context, ILogger<IEmotionService> emotionLoggerService)
+        {
+            _context = context;
+            _emotionLoggerService = emotionLoggerService;
+        }
+        public async Task<Response> Get()
+        {
+            try
+            {
+                var dbEmotions = await _context.Emotions.ToListAsync();
+                if (dbEmotions.Any())
+                {
+                    _emotionLoggerService.LogInformation($"GET: {EmotionServiceLogTypes.SUCCESS.ToString()}", dbEmotions);
+                    return new Response(dbEmotions, StatusCodes.Status200OK, EmotionServiceLogTypes.SUCCESS.ToString());
+                }
+                _emotionLoggerService.LogWarning($"GET: {EmotionServiceLogTypes.EMPTY_OR_NULL_TABLE.ToString()}", dbEmotions);
+                return new Response(dbEmotions, StatusCodes.Status204NoContent, EmotionServiceLogTypes.EMPTY_OR_NULL_TABLE.ToString());
+            }
+            catch (Exception e)
+            {
+                _emotionLoggerService.LogError($"GET: {EmotionServiceLogTypes.ERROR.ToString()}", e);
+                return new Response(e.Data, StatusCodes.Status500InternalServerError, EmotionServiceLogTypes.ERROR.ToString());
+            }
+        }
+    }
+}
