a39cc382c121082a1e90472db1f78be8bf93ed58
sitnic-a <admir.hhs@gmail.com>
2025-02-20 21:19:00 +0100

    Emotions picker implemented

--- MentalHealthBlog.Web/src/components/PickedEmotion.js
+++ MentalHealthBlog.Web/src/components/PickedEmotion.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { setPickedEmotions } from './redux-toolkit/features/emotionSlice'
 
 export const PickedEmotion = (props) => {
+  let dispatch = useDispatch()
+  let { pickedEmotions } = useSelector((store) => store.emotion)
+
   let emotionType = props.emotionType
   let emotion = props.emotion
   return (
@@ -8,6 +13,17 @@ export const PickedEmotion = (props) => {
       className={`add-post-picked-emotion-container emotion-${emotionType}`}
       key={emotion.id}
     >
+      <span
+        className="add-post-picked-emotion-remove-from-list-button"
+        onClick={() => {
+          let pickedEmotionsFromList = [...pickedEmotions].filter(
+            (pickedEmotion) => pickedEmotion.name !== emotion.name
+          )
+          dispatch(setPickedEmotions(pickedEmotionsFromList))
+        }}
+      >
+        X
+      </span>
       {emotion.name}
     </div>
   )
