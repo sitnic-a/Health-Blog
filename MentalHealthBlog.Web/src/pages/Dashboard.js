4e37c5672efbeb82a939a38e28082daabac0db7b
sitnic-a <admir.hhs@gmail.com>
2025-03-03 17:14:08 +0100

    Refreshing token - WIP

--- MentalHealthBlog.Web/src/pages/Dashboard.js
+++ MentalHealthBlog.Web/src/pages/Dashboard.js
@@ -1,39 +1,64 @@
-import { useNavigate } from 'react-router-dom'
-import useFetchLocationState from '../components/custom/hooks/useFetchLocationState'
-import { verifyToken } from '../components/utils/helper-methods/jwt'
-import { AdminDashboard } from './AdminDashboard'
-import { MentalExpertDashboard } from './MentalExpertDashboard'
-import { UserDashboard } from './UserDashboard'
-import { useEffect } from 'react'
-import Cookies from 'js-cookie'
+import { useLocation, useNavigate } from "react-router-dom";
+import useFetchLocationState from "../components/custom/hooks/useFetchLocationState";
+import { verifyToken } from "../components/utils/helper-methods/jwt";
+import { AdminDashboard } from "./AdminDashboard";
+import { MentalExpertDashboard } from "./MentalExpertDashboard";
+import { UserDashboard } from "./UserDashboard";
+import { useEffect } from "react";
+import Cookies from "js-cookie";
+import { useDispatch } from "react-redux";
+import { refreshAccessToken } from "../components/redux-toolkit/features/userSlice";
 
 export const Dashboard = () => {
-  let navigate = useNavigate()
-  let refreshToken = Cookies.get('refreshToken')
-  console.log('Refresh token ', refreshToken)
+  let navigate = useNavigate();
+  let location = useLocation();
+  let dispatch = useDispatch();
+  let refreshToken = Cookies.get("refreshToken");
+  console.log("Refresh token ", refreshToken);
 
-  let { loggedUser } = useFetchLocationState()
+  let { loggedUser } = useFetchLocationState();
 
   useEffect(() => {
     if (!verifyToken(loggedUser.token)) {
-      navigate('login')
+      dispatch(refreshAccessToken(refreshToken)).then((response) => {
+        let authenticatedUser = response.payload.serviceResponseObject;
+        let statusCode = parseInt(response.payload.statusCode);
+        console.log("Authenticated user ", authenticatedUser);
+        if (statusCode === 201 || statusCode === 200) {
+          console.log("Token successfuly refreshed");
+          location.state = {
+            prevUrl: window.location.href,
+            loggedUser: {
+              id: authenticatedUser.id,
+              username: authenticatedUser.username,
+              token: authenticatedUser.jwToken,
+              roles: authenticatedUser.userRoles,
+            },
+          };
+          console.log("Location ", location);
+
+          return;
+        }
+        console.log("Log in again");
+      });
+      // navigate('login')
     }
-  }, [])
+  }, []);
 
   if (loggedUser !== null) {
-    if (loggedUser.roles.some((ur) => ur.name === 'User')) {
-      return <UserDashboard />
+    if (loggedUser.roles.some((ur) => ur.name === "User")) {
+      return <UserDashboard />;
     }
-    if (loggedUser.roles.some((ur) => ur.name === 'Administrator')) {
-      return <AdminDashboard />
+    if (loggedUser.roles.some((ur) => ur.name === "Administrator")) {
+      return <AdminDashboard />;
     }
     if (
       loggedUser.roles.some(
-        (ur) => ur.name === 'Psychologist / Psychotherapist'
+        (ur) => ur.name === "Psychologist / Psychotherapist"
       )
     ) {
-      return <MentalExpertDashboard />
+      return <MentalExpertDashboard />;
     }
   }
-  return <p>ERROR</p>
-}
+  return <p>ERROR</p>;
+};
