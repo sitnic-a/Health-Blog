22203ce8db52b5b26bd1f0dc0e7acae43eb08720
sitnic-a <admir.hhs@gmail.com>
2025-03-03 20:52:01 +0100

    Trying to re-set new authentication token after refreshing - WIP

--- MentalHealthBlog.Web/src/components/utils/helper-methods/jwt.js
+++ MentalHealthBlog.Web/src/components/utils/helper-methods/jwt.js
@@ -1,12 +1,25 @@
 import { jwtDecode } from "jwt-decode";
 
 export const verifyToken = (token) => {
+  const ONE_MINUTE = 60000;
   let decodedToken = jwtDecode(token);
-  let currentDate = new Date();
+  console.log("Decoded token ", decodedToken);
 
-  // JWT exp is in seconds
-  if (decodedToken.exp * 1000 < currentDate.getTime()) {
+  let expireAtEpoch = new Date(decodedToken.exp * 1000).getTime();
+  let expirationDate = new Date(expireAtEpoch).toLocaleString();
+  console.log("Expire at ", expireAtEpoch, expirationDate);
+
+  let currentEpoch = new Date().getTime();
+  let currentDate = new Date(currentEpoch).toLocaleString();
+  console.log("Current ", currentEpoch, currentDate);
+
+  let difference = expireAtEpoch - currentEpoch;
+  console.log("Difference ", difference);
+
+  if (difference < ONE_MINUTE) {
     return false;
   }
   return true;
+
+  // JWT exp is in seconds
 };
