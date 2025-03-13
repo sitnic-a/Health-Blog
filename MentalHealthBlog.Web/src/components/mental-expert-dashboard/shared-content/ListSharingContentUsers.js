b872740913d2218467f427dd4a5ba18b2b18a156
sitnic-a <admir.hhs@gmail.com>
2025-03-13 12:20:29 +0100

    Changes on mental health expert dashboard - refresh tokens

--- MentalHealthBlog.Web/src/components/mental-expert-dashboard/shared-content/ListSharingContentUsers.js
+++ MentalHealthBlog.Web/src/components/mental-expert-dashboard/shared-content/ListSharingContentUsers.js
@@ -1,33 +1,35 @@
-import React from "react";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import useFetchLocationState from "../../custom/hooks/useFetchLocationState";
+import React from 'react'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import useFetchLocationState from '../../custom/hooks/useFetchLocationState'
 import {
   getOnlyUsersThatSharedContent,
   getSharedContentOfPickedUser,
   getSharesPerUser,
   setOverlayPost,
-} from "../../redux-toolkit/features/mentalExpertSlice";
-import { expandShrinkSidebar } from "../../utils/helper-methods/methods";
+} from '../../redux-toolkit/features/mentalExpertSlice'
+import { expandShrinkSidebar } from '../../utils/helper-methods/methods'
 
-import { BiExpandAlt } from "react-icons/bi";
+import { BiExpandAlt } from 'react-icons/bi'
 
 export const ListSharingContentUsers = () => {
-  let dispatch = useDispatch();
-  let { usersThatSharedIncludingItsContent, usersThatSharedContent } =
-    useSelector((store) => store.mentalExpert);
+  let dispatch = useDispatch()
+  let { authenticatedUser } = useSelector((store) => store.user)
 
-  let { loggedUser } = useFetchLocationState();
+  let { usersThatSharedIncludingItsContent, usersThatSharedContent } =
+    useSelector((store) => store.mentalExpert)
+
+  // let { loggedUser } = useFetchLocationState();
 
   let query = {
-    loggedExpertId: loggedUser.id,
-  };
+    loggedExpertId: authenticatedUser.id,
+  }
 
   useEffect(() => {
     dispatch(getSharesPerUser(query)).then((data) => {
-      dispatch(getOnlyUsersThatSharedContent(data));
-    });
-  }, []);
+      dispatch(getOnlyUsersThatSharedContent(data))
+    })
+  }, [])
 
   return (
     <section className="sharing-users-main-users-container">
@@ -35,7 +37,7 @@ export const ListSharingContentUsers = () => {
         <span
           className="sharing-users-expander-icon"
           onClick={() => {
-            expandShrinkSidebar();
+            expandShrinkSidebar()
           }}
         >
           <BiExpandAlt />
@@ -50,27 +52,27 @@ export const ListSharingContentUsers = () => {
                 key={user.id}
                 onClick={() => {
                   if (window.screen.width <= 550) {
-                    expandShrinkSidebar();
+                    expandShrinkSidebar()
                     let contentAndQuery = {
                       userId: user.id,
                       usersThatSharedIncludingItsContent,
-                    };
-                    dispatch(getSharedContentOfPickedUser(contentAndQuery));
-                    dispatch(setOverlayPost(null));
+                    }
+                    dispatch(getSharedContentOfPickedUser(contentAndQuery))
+                    dispatch(setOverlayPost(null))
                   }
                   let contentAndQuery = {
                     userId: user.id,
                     usersThatSharedIncludingItsContent,
-                  };
-                  dispatch(getSharedContentOfPickedUser(contentAndQuery));
-                  dispatch(setOverlayPost(null));
+                  }
+                  dispatch(getSharedContentOfPickedUser(contentAndQuery))
+                  dispatch(setOverlayPost(null))
                 }}
               >
                 <span className="sharing-user-title">{user.username}</span>
               </div>
-            );
+            )
           })}
       </div>
     </section>
-  );
-};
+  )
+}
