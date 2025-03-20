61d554b30457ca1256452e34808ed08d6a73a766
sitnic-a <admir.hhs@gmail.com>
2025-03-20 21:21:37 +0100

    Post service improvement - fetching posts emotions

---
+++ MentalHealthBlog.API/Models/ResourceResponse/EmotionDto.cs
@@ -0,0 +1,14 @@
+﻿namespace MentalHealthBlog.API.Models.ResourceResponse
+{
+    public class EmotionDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public EmotionDto(){}
+        public EmotionDto(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
