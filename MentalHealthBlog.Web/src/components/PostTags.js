793dada9a5cff751e49a0305f830b79a0ba6a6f3
sitnic-a <admir.hhs@gmail.com>
2025-03-22 11:23:38 +0100

    Tag grades erased

--- MentalHealthBlog.Web/src/components/PostTags.js
+++ MentalHealthBlog.Web/src/components/PostTags.js
@@ -1,29 +1,14 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
-import {
-  setIsHovered,
-  setHoveredPost,
-  setHoveredTag,
-} from './redux-toolkit/features/tagGradeSlice'
 import Tag from './Tag'
 
 export const PostTags = (props) => {
-  let dispatch = useDispatch()
-
   let post = props.post
 
   return (
-    <div
-      className="post-container-main-tags"
-      onMouseLeave={() => {
-        dispatch(setIsHovered(false))
-        dispatch(setHoveredPost(null))
-        dispatch(setHoveredTag(null))
-      }}
-    >
+    <div className="post-container-main-tags">
       <div className="post-container-tags">
         {post.tags.map((tag) => {
-          return <Tag key={tag} post={post} tag={tag} />
+          return <Tag key={tag} tag={tag} />
         })}
       </div>
     </div>
