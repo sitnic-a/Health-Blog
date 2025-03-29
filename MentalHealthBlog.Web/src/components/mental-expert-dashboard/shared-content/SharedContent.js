9ffe79c279345d12b7d4481812ea26944795adec
sitnic-a <admir.hhs@gmail.com>
2025-03-29 14:54:35 +0100

    Displayed emotions to mental health expert - WIP

--- MentalHealthBlog.Web/src/components/mental-expert-dashboard/shared-content/SharedContent.js
+++ MentalHealthBlog.Web/src/components/mental-expert-dashboard/shared-content/SharedContent.js
@@ -1,5 +1,7 @@
 import { formatDateToString } from '../../utils/helper-methods/methods'
 
+import { PostEmotions } from '../../PostEmotions'
+
 export const SharedContent = (props) => {
   let content = props.content
   let date = formatDateToString(content.createdAt)
@@ -15,15 +17,24 @@ export const SharedContent = (props) => {
         <p>{content.content}</p>
       </div>
 
-      <div className="sharing-users-post-tags">
-        <span>Tagovi: </span>
-        {content.tags.map((tag) => {
-          return (
-            <span className="sharing-users-post-tag" key={tag}>
-              {tag}
-            </span>
-          )
-        })}
+      <div className="sharing-users-main-post-tags">
+        <div className="sharing-users-post-tags">
+          <span>Tagovi: </span>
+          {content.tags.map((tag) => {
+            return (
+              <span className="sharing-users-post-tag" key={tag}>
+                {tag}
+              </span>
+            )
+          })}
+        </div>
+      </div>
+
+      <div className="sharing-users-main-post-emotions">
+        <div className="sharing-users-post-emotions">
+          <span>Emotions: </span>
+          <PostEmotions post={content} />
+        </div>
       </div>
     </div>
   )
