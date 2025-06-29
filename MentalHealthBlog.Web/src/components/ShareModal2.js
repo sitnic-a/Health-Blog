6fc68ff11f69c78f97de163963b5fa34dd777dd8
sitnic-a <admir.hhs@gmail.com>
2025-06-29 15:52:49 +0200

    WIP

--- MentalHealthBlog.Web/src/components/ShareModal2.js
+++ MentalHealthBlog.Web/src/components/ShareModal2.js
@@ -1,5 +1,10 @@
 import { useDispatch, useSelector } from 'react-redux'
 
+import { LiaSearchSolid } from 'react-icons/lia'
+import defaultAvatar from '../images/default-avatar.png'
+
+import { MdPhone, MdEmail } from 'react-icons/md'
+
 export const ShareModal2 = () => {
   let dispatch = useDispatch()
   let { authenticatedUser } = useSelector((store) => store.user)
@@ -21,17 +26,23 @@ export const ShareModal2 = () => {
             <div className="share-modal-posts-to-share">
               <div className="share-modal-post-to-share-container">
                 <p className="share-modal-post-to-share-title">Test post 1</p>
-                <span>X</span>
+                <span className="share-modal-post-to-share-remove-from-list">
+                  X
+                </span>
               </div>
 
               <div className="share-modal-post-to-share-container">
                 <p className="share-modal-post-to-share-title">Test post 1</p>
-                <span>X</span>
+                <span className="share-modal-post-to-share-remove-from-list">
+                  X
+                </span>
               </div>
 
               <div className="share-modal-post-to-share-container">
                 <p className="share-modal-post-to-share-title">Test post 1</p>
-                <span>X</span>
+                <span className="share-modal-post-to-share-remove-from-list">
+                  X
+                </span>
               </div>
             </div>
           </div>
@@ -42,7 +53,13 @@ export const ShareModal2 = () => {
                 type="text"
                 className="search-by-name-surname-organization"
               />
-              <button type="button">Search</button>
+              <button
+                className="search-by-name-surname-organization-filter-button"
+                type="button"
+              >
+                Search
+                <LiaSearchSolid className="search-by-name-surname-organization-filter-icon" />
+              </button>
             </div>
 
             <div className="share-modal-share-actions">
@@ -54,34 +71,56 @@ export const ShareModal2 = () => {
               </button>
             </div>
           </div>
+
           <div className="share-modal-people-to-share-with">
-            <input type="checkbox" name="person-to-share-content-with" />
-
             <div className="person-to-share-with-main-container">
-              <div className="person-to-share-with-img-container">
-                <img className="person-to-share-with-img" src="" alt="" />
-              </div>
-
-              <div className="person-to-share-with-header-info-container">
-                <h3>Ime i Prezime</h3>
-                <p>Role</p>
-              </div>
-
-              <div className="person-to-share-with-additional-info-container">
-                <div className="person-to-share-with-additional-info-field">
-                  <span>icon</span>
-                  <span>+38762/000-111</span>
+              <input
+                className="person-to-share-content-with-checkbox"
+                type="checkbox"
+                name="person-to-share-content-with-checkbox"
+              />
+              <div className="person-to-share-with-container">
+                <div className="person-to-share-with-img-container">
+                  <img
+                    className="person-to-share-with-img"
+                    src={defaultAvatar}
+                    alt="Person"
+                  />
                 </div>
 
-                <div className="person-to-share-with-additional-info-field">
-                  <span>icon</span>
-                  <span>emailemail123@email.com</span>
+                <div className="person-to-share-with-header-info-container">
+                  <h3 className="person-to-share-with-name">Ime i Prezime</h3>
+                  <p className="person-to-share-with-role">Role</p>
                 </div>
 
-                <hr />
+                <div className="person-to-share-with-additional-info-container">
+                  <div className="person-to-share-with-additional-info-field person-to-share-with-phone-field">
+                    <MdPhone className="person-to-share-with-phone-icon" />
+                    <p
+                      className="person-to-share-with-phone-value"
+                      title="+38762/000-111"
+                    >
+                      +38762/000-111
+                    </p>
+                  </div>
 
-                <div className="person-to-share-with-additional-info-field">
-                  <span>organization</span>
+                  <div className="person-to-share-with-additional-info-field person-to-share-with-email-field">
+                    <MdEmail className="person-to-share-with-email-icon" />
+                    <p
+                      className="person-to-share-with-email-value"
+                      title="emaiemail123@email.com"
+                    >
+                      emailemail123@email.com
+                    </p>
+                  </div>
+
+                  <hr />
+
+                  <div className="person-to-share-with-additional-info-field person-to-share-with-organization-field">
+                    <p className="person-to-share-with-organization-value">
+                      organization
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
