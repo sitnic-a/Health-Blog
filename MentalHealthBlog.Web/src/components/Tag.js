1be5ed15122bf638b06ec64fdda7512138f7acb8
sitnic-a <admir.hhs@gmail.com>
2025-08-13 12:56:52 +0200

    Minor bug fix - Ordering while fetching recent shares

--- MentalHealthBlog.Web/src/components/Tag.js
+++ MentalHealthBlog.Web/src/components/Tag.js
@@ -1,7 +1,5 @@
-import React from 'react'
-
 export const Tag = (props) => {
-  let tag = props.tag
+  let tag = props?.tag
 
   return <div className="add-post-content-picked-tags-span-tag">{tag}</div>
 }
