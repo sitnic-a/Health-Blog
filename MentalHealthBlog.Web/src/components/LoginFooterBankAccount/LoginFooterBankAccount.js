5a342349712632b6c292b9660d0d915b2f3278a0
sitnic-a <admir.hhs@gmail.com>
2025-10-03 11:59:47 +0200

    Changes about bank account

--- MentalHealthBlog.Web/src/components/LoginFooterBankAccount/LoginFooterBankAccount.js
+++ MentalHealthBlog.Web/src/components/LoginFooterBankAccount/LoginFooterBankAccount.js
@@ -6,10 +6,13 @@ export const LoginFooterBankAccount = () => {
       <div className="footer-bank-account-container">
         <div className="footer-bank-account-container-header">
           <p className="footer-bank-account-header-text">
-            Molimo Vas da uplate vršite na račun
+            Ime organizacije: Udruženje Menssana
           </p>
-          <p className="footer-bank-account-header-text-account-value">
-            1541602004560520
+          <p className="footer-bank-account-header-text">
+            Žiro račun: 1541602004560520
+          </p>
+          <p className="footer-bank-account-header-text">
+            Adresa: Branilaca Sarajeva 51, 71000 Sarajevo
           </p>
         </div>
       </div>
