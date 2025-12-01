e859dd0b508a2d86752a75038f10f0babf8eb165
sitnic-a <admir.hhs@gmail.com>
2025-12-01 15:23:19 +0100

    Implemented filtering subscriptions feature on admin side

--- MentalHealthBlog.Web/src/components/administrator-dashboard/sections/admin-subscriptions/AdminSubscriptions.js
+++ MentalHealthBlog.Web/src/components/administrator-dashboard/sections/admin-subscriptions/AdminSubscriptions.js
@@ -9,19 +9,27 @@ import { LiaSearchSolid } from 'react-icons/lia'
 
 import AdminSubscriptionsCSS from './AdminSubscriptions.css'
 import { getSubscriptionUsers } from '../../../../redux-toolkit/features/subscriptionSlice'
+import { stringIsNullOrEmpty } from '../../../../utils/helper-methods/methods'
 
 export const AdminSubscriptions = () => {
   let dispatch = useDispatch()
+  let authenticatedUserLocalStorage = localStorage.getItem('authenticatedUser')
+  let authenticatedUser = JSON.parse(authenticatedUserLocalStorage)
+
   let { subscriptionUsers } = useSelector((store) => store.subscription)
   let [userTypes, setUserTypes] = useState([])
   let [months, setMonths] = useState([])
 
   useEffect(() => {
-    dispatch(getSubscriptionUsers())
+    let objectWithData = {
+      authenticatedUser,
+    }
+    dispatch(getSubscriptionUsers(objectWithData))
     moment.locale('bs')
     let bsMonthsWithFirstUppercaseLetter = moment.months().map((month) => {
       return month.charAt(0).toUpperCase() + month.slice(1)
     })
+    bsMonthsWithFirstUppercaseLetter.unshift('Odaberite mjesec')
     setMonths(bsMonthsWithFirstUppercaseLetter)
     dispatch(getDbRoles()).then((data) => {
       let statusCode = data?.payload?.statusCode
@@ -70,7 +78,7 @@ export const AdminSubscriptions = () => {
                       <option
                         className="admin-subscriptions-header-filter-month-value"
                         key={index + 1}
-                        value={index + 1}
+                        value={index}
                       >
                         {month}
                       </option>
@@ -107,7 +115,50 @@ export const AdminSubscriptions = () => {
               </div>
 
               <div className="admin-subscriptions-header-filter-col">
-                <button className="admin-subscriptions-header-filter-search-button">
+                <button
+                  className="admin-subscriptions-header-filter-search-button"
+                  onClick={() => {
+                    let subscriptionYearValue = document.querySelector(
+                      '.admin-subscriptions-header-filter-year-value'
+                    ).value
+                    let subscriptionYear = 0
+                    let subscriptionMonth = document.querySelector(
+                      '.admin-subscriptions-filter-months-picker'
+                    ).value
+                    let userTypeId = document.querySelector(
+                      '.admin-subscriptions-header-filter-user-types-picker'
+                    ).value
+                    let firstNameLastNameUsername = document.querySelector(
+                      '.admin-subscriptions-header-filter-name-surname-username-value'
+                    ).value
+
+                    if (!stringIsNullOrEmpty(subscriptionYearValue)) {
+                      subscriptionYear = parseInt(subscriptionYearValue)
+                    }
+                    console.log(
+                      'SubYear ',
+                      subscriptionYear,
+                      ' SubMonth ',
+                      subscriptionMonth,
+                      ' UserId ',
+                      userTypeId,
+                      ' NameUser ',
+                      firstNameLastNameUsername
+                    )
+
+                    let query = {
+                      subscriptionYear: subscriptionYear,
+                      subscriptionMonth: parseInt(subscriptionMonth),
+                      userTypeId: parseInt(userTypeId),
+                      firstNameLastNameUsername: firstNameLastNameUsername,
+                    }
+                    let objectWithData = {
+                      query,
+                      authenticatedUser,
+                    }
+                    dispatch(getSubscriptionUsers(objectWithData))
+                  }}
+                >
                   Tražite{' '}
                   <LiaSearchSolid className="admin-subscriptions-header-filter-search-button-icon" />
                 </button>
@@ -137,23 +188,42 @@ export const AdminSubscriptions = () => {
 
             <tbody className="admin-subscriptions-subscription-table-body">
               {/* users list */}
-              {subscriptionUsers?.map((subscriptionUser) => {
+              {subscriptionUsers?.map((subscriptionUser, index) => {
                 return (
-                  <tr className="admin-subscription-subscription-table-body-row">
-                    <td className="admin-subscription-subscription-table-body-cell">
+                  <tr
+                    key={index + 1}
+                    className="admin-subscriptions-subscription-table-body-row"
+                  >
+                    <td className="admin-subscriptions-subscription-table-body-cell">
                       {subscriptionUser?.firstName}
                     </td>
-                    <td className="admin-subscription-subscription-table-body-cell">
+                    <td className="admin-subscriptions-subscription-table-body-cell">
                       {subscriptionUser?.lastName}
                     </td>
-                    <td className="admin-subscription-subscription-table-body-cell">
+                    <td className="admin-subscriptions-subscription-table-body-cell">
                       {subscriptionUser?.username}
                     </td>
-                    <td className="admin-subscription-subscription-table-body-cell">
-                      <span>manje od 2 dana</span>
-                      <input type="text" placeholder="Unesite iznos uplate" />
-                      <button type="button">Uplatite</button>
-                      <button type="button">Zabrana</button>
+                    <td className="admin-subscriptions-subscription-table-body-cell">
+                      <span className="admin-subscriptions-subscription-table-body-cell-remaining-days-reminder">
+                        manje od 2 dana
+                      </span>
+                      <input
+                        className="admin-subscriptions-subscription-table-body-cell-payment-value"
+                        type="text"
+                        placeholder="Unesite iznos uplate"
+                      />
+                      <button
+                        className="admin-subscriptions-subscription-table-body-cell-action admin-subscription-subscription-enable"
+                        type="button"
+                      >
+                        Uplatite
+                      </button>
+                      <button
+                        className="admin-subscriptions-subscription-table-body-cell-action admin-subscription-subscription-disable"
+                        type="button"
+                      >
+                        Zabrana
+                      </button>
                     </td>
                   </tr>
                 )
