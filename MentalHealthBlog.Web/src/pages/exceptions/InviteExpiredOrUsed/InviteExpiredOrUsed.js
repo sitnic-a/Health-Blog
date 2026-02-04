6dff0b1bde26f12df8b34ccc525eee51a61a24aa
sitnic-a <admir.hhs@gmail.com>
2026-02-04 21:03:01 +0100

    Fixed bug - expired / used invitation

--- MentalHealthBlog.Web/src/pages/exceptions/InviteExpiredOrUsed/InviteExpiredOrUsed.js
+++ MentalHealthBlog.Web/src/pages/exceptions/InviteExpiredOrUsed/InviteExpiredOrUsed.js
@@ -1,3 +1,29 @@
+import { TbClockExclamation } from 'react-icons/tb'
+
+import InviteExpiredOrUsedCSS from './InviteExpiredOrUsed.css'
+
 export const InviteExpiredOrUsed = () => {
-  return <div>InviteExpiredOrUsed</div>
+  return (
+    <section id="invitation-expired-or-used-main-container">
+      <div className="invitation-expired-or-used-container">
+        <div className="invitation-expired-or-used-content">
+          <TbClockExclamation className="invitation-expired-or-used-icon" />
+          <p className="invitation-expired-or-used-title">
+            Pozivnica nije validna!
+          </p>
+          <p className="invitation-expired-or-used-description">
+            Tražena pozivnica je istekla ili je već prethodno iskorištena.
+          </p>
+          <div className="invitation-expired-or-used-actions">
+            <a
+              href="https://mapp-terapija.com/"
+              className="invitation-expired-or-used-go-to-app-button"
+            >
+              Idi na aplikaciju
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
